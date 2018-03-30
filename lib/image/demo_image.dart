68f0c6e2ff54aba376f5c537bbfa38606d9c582b
Ceyx <lh_halcyondays@163.com>
2018-03-30 14:55:24 +0800

    todo more about image

--- lib/image/demo_image.dart
+++ lib/image/demo_image.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 
 class DemoImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var title = 'Demo Image';
+    var imgUrl = 'https://ws1.sinaimg.cn/large/006tNc79gy1fpa5bvsqskj3044048mx5.jpg';
+    var assetsImg = 'lib/resources/images/gender.png';
+
 
     return new Scaffold(
         appBar: new AppBar(
@@ -12,9 +16,39 @@ class DemoImage extends StatelessWidget {
         ),
         body: new Column(
           children: <Widget>[
-            new Image.network(
-              'https://github.com/flutter/website/blob/master/_includes/code/layout/lakes/images/lake.jpg?raw=true',
+            new Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: new Card(
+                  child: new Row(
+                    children: <Widget>[
+                      const Text('FadeInImage:'),
+                      new FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: imgUrl,
+                        width: 150.0,
+                        height: 150.0,
+                        fadeInDuration: const Duration(milliseconds: 1200),
+                        fadeOutDuration: const Duration(milliseconds: 1200),
+                      )
+                    ],
+                  ),
+                )
             ),
+            new Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: new Card(
+                  child: new Row(
+                    children: <Widget>[
+                      const Text('Image.asset:'),
+                      new Image.asset(
+                        assetsImg,
+                        width: 150.0,
+                        height: 150.0,
+                      )
+                    ],
+                  ),
+                )
+            )
           ],
         ));
   }
