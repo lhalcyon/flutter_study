4b92ddd121716f6d0e715c1a3964fed76a4e322d
Ceyx <lh_halcyondays@163.com>
2018-03-30 15:29:46 +0800

    todo image file

--- lib/image/demo_image.dart
+++ lib/image/demo_image.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
 
@@ -7,14 +9,59 @@ class DemoImage extends StatelessWidget {
   Widget build(BuildContext context) {
     var title = 'Demo Image';
     var imgUrl = 'https://ws1.sinaimg.cn/large/006tNc79gy1fpa5bvsqskj3044048mx5.jpg';
-    var assetsImg = 'resources/images/ic_link.png';
+    var imageAssets = 'resources/images/ic_link.png';
 
     return new Scaffold(
         appBar: new AppBar(
           title: new Text(title),
         ),
-        body: new Column(
+        body: new ListView(
           children: <Widget>[
+            new Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: new Card(
+                  child: new Row(
+                    children: <Widget>[
+                      const Text('Image.asset:'),
+                      new Image.asset(
+                        imageAssets,
+                        width: 150.0,
+                        height: 150.0,
+                      )
+                    ],
+                  ),
+                )
+            ),
+            new Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: new Card(
+                  child: new Row(
+                    children: <Widget>[
+                      const Text('image.network:'),
+                      new Image.network(
+                        imgUrl,
+                        width: 150.0,
+                        height: 150.0,
+                      )
+                    ],
+                  ),
+                )
+            ),
+//            new Padding(
+//                padding: const EdgeInsets.all(16.0),
+//                child: new Card(
+//                  child: new Row(
+//                    children: <Widget>[
+//                      const Text('image.file:'),
+//                      new Image.file(
+//                        new File('resources/images/tab_n.png'),
+//                        width: 150.0,
+//                        height: 150.0,
+//                      )
+//                    ],
+//                  ),
+//                )
+//            ),
             new Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: new Card(
@@ -33,21 +80,6 @@ class DemoImage extends StatelessWidget {
                   ),
                 )
             ),
-            new Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: new Card(
-                  child: new Row(
-                    children: <Widget>[
-                      const Text('Image.asset:'),
-                      new Image.asset(
-                        assetsImg,
-                        width: 150.0,
-                        height: 150.0,
-                      )
-                    ],
-                  ),
-                )
-            )
           ],
         ));
   }
