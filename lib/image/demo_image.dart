7570c81ae20206d42fb0541bea4a8ab86ede6161
lhalcyon <lh_halcyondays@163.com>
2018-04-03 22:41:01 +0800

    todo 三方包assets

--- lib/image/demo_image.dart
+++ lib/image/demo_image.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
-
+import 'package:material_icons/material_icons.dart';
 
 class DemoImage extends StatelessWidget {
   @override
@@ -10,7 +10,7 @@ class DemoImage extends StatelessWidget {
     var title = 'Demo Image';
     var imgUrl = 'https://ws1.sinaimg.cn/large/006tNc79gy1fpa5bvsqskj3044048mx5.jpg';
     var imageAssets = 'assets/images/ic_link.png';
-
+    var imageFromPackage = 'assets/sprites/css-sprite/sprite-action-black.png';
     return new Scaffold(
         appBar: new AppBar(
           title: new Text(title),
@@ -88,6 +88,21 @@ class DemoImage extends StatelessWidget {
                       const Text('ImageProvider Network:'),
                       new Image(
                         image: new NetworkImage(imgUrl),
+                        width: 150.0,
+                        height: 150.0,
+                      )
+                    ],
+                  ),
+                )
+            ),
+            new Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: new Card(
+                  child: new Row(
+                    children: <Widget>[
+                      const Text('Image from package:'),
+                      new Image(
+                        image: new AssetImage(imageFromPackage),
                         width: 150.0,
                         height: 150.0,
                       )
